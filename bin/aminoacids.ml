a7c0157800475c3373ae6f05399a918f26551bdc
DigitalChemistNM <2885282M@student.gla.ac.uk>
2024-05-13 16:16:18 +0100

    added more functions including smiles generations and monoisotopic mass calculation

--- bin/aminoacids.ml
+++ bin/aminoacids.ml
@@ -13,7 +13,8 @@ type amino_acid =
 
  
 
-let parse_line line = 
+(*function for parsing lines of the CSV file and creating an amino acid type with the info populated*)
+  let parse_line line = 
   match String.split_on_char ',' line with 
   | [number; name; three_letter_code; one_letter_code; monoisotopic_mass; average_mass; formula; smiles ] ->
     {
@@ -29,6 +30,7 @@ let parse_line line =
     | _ -> failwith "Invalid" 
 
 
+(*function to read in CSV*)
 let read_csv filename =
   let channel = open_in filename in
   let rec read_lines acc =
@@ -41,6 +43,7 @@ let read_csv filename =
     in
       read_lines []
 
+(*create a list of amino_acids that comprise of the 20 naturally ocurring ones*)
 let natural_amino_acids = read_csv "aa.csv"  
 
 let rec find_amino_acid_by_one_letter_code code amino_acid_list = 
@@ -52,8 +55,8 @@ let rec find_amino_acid_by_one_letter_code code amino_acid_list =
     else 
       find_amino_acid_by_one_letter_code code rest 
       
-(*let _ = find_amino_acid_by_one_letter_code 'A' natural_amino_acids*)
 
+(*find the mass of a paricular amino acid by its one letter code*)
 let rec find_average_mass_by_one_letter_code code amino_acid_list = 
   match amino_acid_list with 
   | [] -> raise Not_found
@@ -63,13 +66,31 @@ let rec find_average_mass_by_one_letter_code code amino_acid_list =
       else 
         find_average_mass_by_one_letter_code code rest 
 
-let iterate_through_string str = 
+let rec find_monoisotopic_mass_by_one_letter_code code amino_acid_list = 
+  match amino_acid_list with 
+  | [] -> raise Not_found
+  | aa :: rest -> 
+      if aa.one_letter_code = code then 
+        aa.monoisotopic_mass
+      else 
+        find_monoisotopic_mass_by_one_letter_code code rest
+
+let rec find_smiles_by_one_letter_code code amino_acid_list = 
+  match amino_acid_list with 
+  | [] -> raise Not_found
+  | aa :: rest -> 
+      if aa.one_letter_code = code then 
+        aa.smiles
+      else 
+        find_smiles_by_one_letter_code code rest
+
+(*let iterate_through_string str = 
   let len = String.length str in 
   for i = 0 to len -1 do 
     let current_char = str.[i] in 
     print_char current_char;
     print_newline() 
-  done
+  done*)
 
 let calculate_mass sequence = 
   let mass = ref 0.0 in
@@ -80,10 +101,42 @@ let calculate_mass sequence =
     mass := !mass +. found_mass;
   done;
   mass := !mass +. 18.01528;
+  !mass 
+
+let calculate_monoisotopic_mass sequence = 
+  let mass = ref 0.0 in
+  let len = String.length sequence in 
+  for i = 0 to len -1 do 
+    let current_char = sequence.[i] in 
+    let found_mass = find_monoisotopic_mass_by_one_letter_code current_char natural_amino_acids in 
+    mass := !mass +. found_mass;
+  done;
+  mass := !mass +. 18.0105;
   !mass
+
+let remove_last_char str = 
+  let len = String.length str in 
+  let new_str = String.sub str 0 (len - 1) in 
+  new_str 
+
+let generate_smiles sequence = 
+  let smiles = ref "" in 
+  let len = String.length sequence in 
+  for i = 0 to len -1 do 
+    let current_char = sequence.[i] in 
+    let found_smiles = find_smiles_by_one_letter_code current_char natural_amino_acids in 
+    smiles := !smiles ^ remove_last_char(found_smiles);
+  done;
+  smiles := !smiles ^ "O";
+  !smiles
 
 let peptide_1 = "NICKISEPIC"
 
 let x = calculate_mass peptide_1 
 
-let () = print_float x
+let y = calculate_monoisotopic_mass peptide_1 
+
+let z = generate_smiles peptide_1
+
+let () = print_float x; print_newline(); print_float y; print_newline(); print_string z; print_newline() 
+
