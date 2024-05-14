a0004507f96cd4ecd271041513a42f7add53efe4
DigitalChemistNM <2885282M@student.gla.ac.uk>
2024-05-14 11:17:48 +0100

    fiex previous issue

--- _build/default/bin/syntax.ml
+++ _build/default/bin/syntax.ml
@@ -11,18 +11,12 @@ type expression =
 
 
 
-let rec print_expr e = 
+let rec eval_expr e = 
   match e with
-  | Sequence (e1, e2) -> print_expr e1; print_expr e2
+  | Sequence (e1, e2) -> eval_expr e1; eval_expr e2
   | Peptide (s, t) -> print_string s; print_string " "; print_string t; print_newline()
   | Molecule (s, t) -> print_string s; print_string " "; print_string t; print_newline()
   | Solvent (s)  -> print_string s; print_newline()
   | Solution (s, t, f, l) -> print_string s; print_string " "; print_string t; print_string " "; print_float f; print_string " "; print_string l; print_newline()
-  | CalculateAverageMass (s) -> print_string s; print_newline()
-  | GenerateSmiles (s) -> print_string s; print_newline() 
-
-let eval_expr e = 
-  match e with
   | CalculateAverageMass (s) -> let x = calculate_mass s in print_float x; print_newline()
   | GenerateSmiles (s) -> let x = generate_smiles s in print_string x; print_newline()
-    | _ -> print_expr e 
